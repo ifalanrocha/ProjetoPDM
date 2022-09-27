2ef44b0b1c17d68b4a0c3a2a1b886d05eeb99553
Alan Rocha <ifalanrocha@gmail.com>
2022-09-26 22:30:18 -0300

    Projeto PDM para turma de ADS Fatec noturno.

--- ProjetoPDM/src/projetoPDM/views/Menu.java
+++ ProjetoPDM/src/projetoPDM/views/Menu.java
@@ -5,11 +5,15 @@ import javax.swing.JOptionPane;
 public class Menu {
      
     public static void Principal() throws SQLException, ClassNotFoundException {
-        int num = 1;
+        int num = 10;
         while (num != 0) {
-        String msg = " MENU PRINCIPAL\n\nINSIRA UMA OPÇÃO:\n0 - SAIR \n1 - USUARIO \n2 - PESSOA \n3 - USUARIO/PESSOA \n4 - LOGRADOURO \n5 - PESSOAALUNO \n6 - DISCIPLINA \n7 - DISCIPLINAALUNO \n8 - ALUNO";
-        num = Integer.parseInt(JOptionPane.showInputDialog(msg));
-
+        String msg = "MENU PRINCIPAL\n\nINSIRA UMA OPÇÃO:\n0 - SAIR \n1 - USUARIO \n2 - PESSOA \n3 - USUARIO/PESSOA \n4 - LOGRADOURO \n5 - PESSOAALUNO \n6 - DISCIPLINA \n7 - DISCIPLINAALUNO \n8 - ALUNO";
+        if (JOptionPane.showInputDialog(msg) == null){
+            
+            JOptionPane.showMessageDialog(null, "Cancelado");
+                break;
+                
+        }else{
         switch (num) {
             case 0:
                 int sair = JOptionPane.showConfirmDialog(null,"DESEJA SAIR?");
@@ -26,16 +30,37 @@ public class Menu {
                 break;
             case 4:
                 mLogradouro();
-                break;    
+                break;
+            case 5:
+                mPessoaAluno();
+                break;
+            case 6:
+                mDisciplina();
+                break;
+            case 7:
+                mDisciplinaAluno();
+                break;
+            case 8:
+                mAluno();
+                break;
+                
             default:
-                System.out.println("OPÇÃO INVÁLIDA");
+                JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA");
         }
+
+      }
+   }
 }
-    }
-    
         public static void mPessoa() throws SQLException, ClassNotFoundException {
-        String msg = " MENU PESSOA\n\nINSIRA UMA OPÇÃO:\n1 - INSERIR \n2 - ALTERAR \n3 - BUSCAR \n4 - EXCLUIR \n5 - LISTAR \n6 - VOLTAR" ;
-        int num = Integer.parseInt(JOptionPane.showInputDialog(msg));
+        int num = 1;
+        while (num != 0) {
+        String msg = " MENU PESSOA\n\nINSIRA UMA OPÇÃO:\n1 - INSERIR \n2 - ALTERAR \n3 - BUSCAR \n4 - EXCLUIR \n5 - LISTAR \n0 - VOLTAR" ;
+        if  (JOptionPane.showInputDialog(msg) == null){
+            
+            JOptionPane.showMessageDialog(null, "Cancelado");
+                break;
+                
+        }else{
         switch (num) {
             case 1:
                 ManterPessoa.inserir();
@@ -56,12 +81,21 @@ public class Menu {
                 Principal();
                 break;
             default:
-                System.out.println("OPÇÃO INVÁLIDA");
+                JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA");
+            }
         }
     }
+}
         public static void mUsuario() throws SQLException, ClassNotFoundException {
+        int num = 1;
+        while (num != 0) {
         String msg = " MENU USUARIO\n\nINSIRA UMA OPÇÃO:\n1 - INSERIR \n2 - ALTERAR \n3 - BUSCAR \n4 - EXCLUIR \n5 - LISTAR \n6 - VOLTAR" ;
-        int num = Integer.parseInt(JOptionPane.showInputDialog(msg));
+        if (JOptionPane.showInputDialog(msg) == null){
+            
+            JOptionPane.showMessageDialog(null, "Cancelado");
+                break;
+                
+        }else{
         switch (num) {
             case 1:
                 ManterUsuario.inserir();
@@ -82,12 +116,21 @@ public class Menu {
                 Principal();
                 break;
             default:
-                System.out.println("OPÇÃO INVÁLIDA");
+                JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA");
+            }
         }
     }
-        public static void mUsuarioPessoa() throws SQLException, ClassNotFoundException {
+}
+        public static void mUsuarioPessoa() throws SQLException, ClassNotFoundException {        
+        int num = 1;
+        while (num != 0) {
         String msg = " MENU USUARIO PESSOA\n\nINSIRA UMA OPÇÃO:\n1 - INSERIR \n2 - ALTERAR \n3 - BUSCAR \n4 - EXCLUIR \n5 - LISTAR \n6 - VOLTAR" ;
-        int num = Integer.parseInt(JOptionPane.showInputDialog(msg));
+        if (JOptionPane.showInputDialog(msg) == null){
+            
+            JOptionPane.showMessageDialog(null, "Cancelado");
+                break;
+                
+        }else{
         switch (num) {
             case 1:
                 ManterUsuarioPessoa.inserir();
@@ -108,13 +151,22 @@ public class Menu {
                 Principal();
                 break;
             default:
-                System.out.println("OPÇÃO INVÁLIDA");
+                JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA");
+            }
         }
     }
+}
         
         public static void mLogradouro() throws SQLException, ClassNotFoundException {
+        int num = 1;
+        while (num != 0) {
         String msg = " MENU PESSOA\n\nINSIRA UMA OPÇÃO:\n1 - INSERIR \n2 - ALTERAR \n3 - BUSCAR \n4 - EXCLUIR \n5 - LISTAR \n6 - VOLTAR" ;
-        int num = Integer.parseInt(JOptionPane.showInputDialog(msg));
+        if (JOptionPane.showInputDialog(msg) == null){
+            
+            JOptionPane.showMessageDialog(null, "Cancelado");
+                break;
+                
+        }else{
         switch (num) {
             case 1:
                 ManterLogradouro.inserir();
@@ -135,7 +187,149 @@ public class Menu {
                 Principal();
                 break;
             default:
-                System.out.println("OPÇÃO INVÁLIDA");
+                JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA");
+            }
+        }
+    }
+}
+        public static void mPessoaAluno() throws SQLException, ClassNotFoundException {
+        int num = 1;
+        while (num != 0) {
+        String msg = " MENU PESSOA\n\nINSIRA UMA OPÇÃO:\n1 - INSERIR \n2 - ALTERAR \n3 - BUSCAR \n4 - EXCLUIR \n5 - LISTAR \n6 - VOLTAR" ;
+        if (JOptionPane.showInputDialog(msg) == null){
+            
+            JOptionPane.showMessageDialog(null, "Cancelado");
+                break;
+                
+        }else{
+        switch (num) {
+            case 1:
+                ManterLogradouro.inserir();
+                break;
+            case 2:
+                ManterLogradouro.alterar();
+                break;
+            case 3:
+                ManterLogradouro.buscar();
+                break;
+            case 4:
+                ManterLogradouro.excluir();
+                break;
+            case 5:
+                ManterLogradouro.listar();
+                break;
+            case 6:
+                Principal();
+                break;
+            default:
+                JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA");
+            }
+        }
+    }
+}
+        public static void mDisciplina() throws SQLException, ClassNotFoundException {
+        int num = 1;
+        while (num != 0) {
+        String msg = " MENU PESSOA\n\nINSIRA UMA OPÇÃO:\n1 - INSERIR \n2 - ALTERAR \n3 - BUSCAR \n4 - EXCLUIR \n5 - LISTAR \n6 - VOLTAR" ;
+        if (JOptionPane.showInputDialog(msg) == null){
+            
+            JOptionPane.showMessageDialog(null, "Cancelado");
+                break;
+                
+        }else{
+        switch (num) {
+            case 1:
+                ManterLogradouro.inserir();
+                break;
+            case 2:
+                ManterLogradouro.alterar();
+                break;
+            case 3:
+                ManterLogradouro.buscar();
+                break;
+            case 4:
+                ManterLogradouro.excluir();
+                break;
+            case 5:
+                ManterLogradouro.listar();
+                break;
+            case 6:
+                Principal();
+                break;
+            default:
+                JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA");
+            }
+        }
+    }
+}
+        public static void mDisciplinaAluno() throws SQLException, ClassNotFoundException {
+        int num = 1;
+        while (num != 0) {
+        String msg = " MENU PESSOA\n\nINSIRA UMA OPÇÃO:\n1 - INSERIR \n2 - ALTERAR \n3 - BUSCAR \n4 - EXCLUIR \n5 - LISTAR \n6 - VOLTAR" ;
+        if (JOptionPane.showInputDialog(msg) == null){
+            
+            JOptionPane.showMessageDialog(null, "Cancelado");
+                break;
+                
+        }else{
+        switch (num) {
+            case 1:
+                ManterLogradouro.inserir();
+                break;
+            case 2:
+                ManterLogradouro.alterar();
+                break;
+            case 3:
+                ManterLogradouro.buscar();
+                break;
+            case 4:
+                ManterLogradouro.excluir();
+                break;
+            case 5:
+                ManterLogradouro.listar();
+                break;
+            case 6:
+                Principal();
+                break;
+            default:
+                JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA");
+            }
+        }
+    }
+}
+        public static void mAluno() throws SQLException, ClassNotFoundException {
+        int num = 1;
+        while (num != 0) {
+        String msg = " MENU PESSOA\n\nINSIRA UMA OPÇÃO:\n1 - INSERIR \n2 - ALTERAR \n3 - BUSCAR \n4 - EXCLUIR \n5 - LISTAR \n6 - VOLTAR" ;
+        if (JOptionPane.showInputDialog(msg) == null){
+            
+            JOptionPane.showMessageDialog(null, "Cancelado");
+                break;
+                
+        }else{
+        switch (num) {
+            case 1:
+                ManterLogradouro.inserir();
+                break;
+            case 2:
+                ManterLogradouro.alterar();
+                break;
+            case 3:
+                ManterLogradouro.buscar();
+                break;
+            case 4:
+                ManterLogradouro.excluir();
+                break;
+            case 5:
+                ManterLogradouro.listar();
+                break;
+            case 6:
+                Principal();
+                break;
+            default:
+                JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA");
+                }
+            }
         }
     }
 }

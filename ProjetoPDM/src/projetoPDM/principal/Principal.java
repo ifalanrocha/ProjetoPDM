25b2c1fb4cc0ecb41fb02bab54972ebf3afa1a86
Alan Rocha <ifalanrocha@gmail.com>
2022-10-03 21:49:35 -0300

    Manutenção no menu principal, limpeza do lixo.

--- ProjetoPDM/src/projetoPDM/principal/Principal.java
+++ ProjetoPDM/src/projetoPDM/principal/Principal.java
@@ -1,12 +1,12 @@
 package projetoPDM.principal;
 
-import projetoPDM.views.Menu;
 import java.sql.SQLException;
+import projetoPDM.views.Menu;
 
 public class Principal {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-            Menu.Principal();
+    public static void main(String[] args) throws SQLException, ClassNotFoundException { 
+        Menu.Principal();
     }
 }
     

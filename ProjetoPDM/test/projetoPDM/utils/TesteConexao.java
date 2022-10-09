54f9a94c431d1bf728b916783b1d514526a8d572
Alan Rocha <ifalanrocha@gmail.com>
2022-10-09 09:37:05 -0300

    Limpando codigo.

--- ProjetoPDM/test/projetoPDM/utils/TesteConexao.java
+++ ProjetoPDM/test/projetoPDM/utils/TesteConexao.java
@@ -1,19 +1,13 @@
 
 package projetoPDM.utils;
 
-import projetoPDM.utils.ConexaoDb;
-
 
 public class TesteConexao {
     
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
         System.out.println(ConexaoDb.statusConection());
         ConexaoDb.getConexaoMySQL();
         System.out.println(ConexaoDb.statusConection());
-
     }   
 }

119f16f108b33a96106bf2e66eddcd32de571682
Alan Rocha <ifalanrocha@gmail.com>
2022-10-08 21:02:45 -0300

    arrumando bd

--- ProjetoPDM/test/projetoPDM/utils/dbDispositivosM.sql
+++ ProjetoPDM/test/projetoPDM/utils/dbDispositivosM.sql
@@ -1,56 +1,63 @@
-create database dbDispositivosM;
-DROP database dbdispositivosm;
+CREATE DATABASE dbDispositivosM;
+DROP DATABASE dbdispositivosM;
 
-create table dbDispositivosM.usuario (
+
+
+
+CREATE TABLE dbDispositivosM.usuario (
   idusu INT NOT NULL AUTO_INCREMENT,
   login VARCHAR(255),
   senha VARCHAR(255),
   status VARCHAR(255),
   tipo VARCHAR(255),
-  primary key (idusu));
+  PRIMARY KEY (idusu));
 
-  INSERT dbDispositivosM.usuario (login, senha, status, tipo) VALUES ('ALAN', '332', 'ADM', 'OOL');
-  insert escola.usuario (email, senha) values ('admin', 'admin');
-  select * from dbDispositivosM.usuario;
-  DROP TABLE dbDispositivosM.usuario;
+    INSERT INTO dbDispositivosM.usuario (login, senha, status, tipo) VALUES ('1', '1', 'ADM', 'ADM');
+    SELECT * FROM dbDispositivosM.usuario;
+    DROP TABLE dbDispositivosM.usuario;
 
 
-create table dbDispositivosM.aluno (
+
+
+CREATE TABLE dbDispositivosM.aluno (
   idalu INT NOT NULL AUTO_INCREMENT,
   nome VARCHAR(255),
   ra VARCHAR(255),
   ano VARCHAR(255),
   idade VARCHAR(255),
-  primary key (idalu));
+  PRIMARY KEY (idalu));
 
-INSERT INTO dbDispositivosM.aluno (idalu, nome, ra, ano, idade) VALUES ('12', 'ALAN', '47474747', '2000', '12');
-  select * from dbDispositivosM.aluno;
-  DROP TABLE dbDispositivosM.aluno;
+    INSERT INTO dbDispositivosM.aluno (idalu, nome, ra, ano, idade) VALUES ('1', 'ALAN', '47474747', '2000', '30');
+    SELECT * FROM dbDispositivosM.aluno;
+    DROP TABLE dbDispositivosM.aluno;
 
 
-create table dbDispositivosM.boletim (
+
+
+CREATE TABLE dbDispositivosM.boletim (
   idbol INT NOT NULL AUTO_INCREMENT,
   semestre INT,
   nmat INT,
   npor INT,
   ngeo INT,
-  primary key (idbol));
+  PRIMARY KEY (idbol));
 
-  INSERT INTO dbDispositivosM.boletim (idbol, idusuario, idpessoa, obs, usu, pes) VALUES ('1', '3', '4', 'oi', 'alan', 'rocha');
-  select * from dbDispositivosM.boletim;
+  INSERT INTO dbDispositivosM.boletim (idbol, semestre, nmat, npor, ngeo) VALUES (1, 2, 3, 4, 5);
+  SELECT * FROM dbDispositivosM.boletim;
   DROP TABLE dbDispositivosM.boletim;
 
 
 
-create table dbDispositivosM.rc (
-   idrc int PRIMARY KEY AUTO_INCREMENT,
-   idusu int,
-   idalu int,
-   idbol int,
+
+CREATE TABLE dbDispositivosM.rc (
+   idrc INT PRIMARY KEY AUTO_INCREMENT,
+   idusu INT,
+   idalu INT,
+   idbol INT,
    FOREIGN KEY (idusu) REFERENCES dbDispositivosM.usuario(idusu),
    FOREIGN KEY (idalu) REFERENCES dbDispositivosM.aluno(idalu),
    FOREIGN KEY (idbol) REFERENCES dbDispositivosM.boletim (idbol));
 
-  INSERT INTO dbDispositivosM.rc (idrc, idusu, idalu, idbol) VALUES ('1', '3', '4', '2');
-  select * from dbDispositivosM.boletim;
+  INSERT INTO dbDispositivosM.rc (idusu, idalu, idbol) VALUES (1, 1, 1);
+  SELECT * FROM dbDispositivosM.rc;
   DROP TABLE dbDispositivosM.rc;
